0273dffa4c767b321a08c44e60d3b5f99061b85a
VukV <vuk.vukovic.2000@gmail.com>
2022-05-31 13:24:00 +0200

    database connection class

--- src/main/java/com/example/vvukovic9420rn_projekat/repositories/Postgres.java
+++ src/main/java/com/example/vvukovic9420rn_projekat/repositories/Postgres.java
@@ -1,4 +1,65 @@
 package com.example.vvukovic9420rn_projekat.repositories;
 
+import java.sql.*;
+import java.util.Optional;
+
 public class Postgres {
+
+    public Postgres(){
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected Connection newConnection() throws SQLException {
+        return DriverManager.getConnection(
+                "jdbc:postgresql://" + this.getHost() + ":" + this.getPort() + "/" + this.getDatabaseName(), this.getUsername(), this.getPassword()
+        );
+    }
+
+    protected String getHost() {
+        return "localhost";
+    }
+
+    protected int getPort() {
+        return 5432;
+    }
+
+    protected String getDatabaseName() {
+        return "web_project";
+    }
+
+    protected String getUsername() {
+        return "postgres";
+    }
+
+    protected String getPassword() {
+        return "vuk123";
+    }
+
+    protected void closeStatement(Statement statement) {
+        try {
+            Optional.of(statement).get().close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void closeResultSet(ResultSet resultSet) {
+        try {
+            Optional.of(resultSet).get().close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void closeConnection(Connection connection) {
+        try {
+            Optional.of(connection).get().close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
