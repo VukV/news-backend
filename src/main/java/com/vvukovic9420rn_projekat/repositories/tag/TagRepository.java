80230a543acf5e1078ece78c40ac46dca69c0c3c
VukV <vuk.vukovic.2000@gmail.com>
2022-05-31 16:54:46 +0200

    added paths and tag repo class

---
+++ src/main/java/com/vvukovic9420rn_projekat/repositories/tag/TagRepository.java
@@ -0,0 +1,4 @@
+package com.vvukovic9420rn_projekat.repositories.tag;
+
+public interface TagRepository {
+}
--- src/main/java/com/vvukovic9420rn_projekat/resources/UserResource.java
+++ src/main/java/com/vvukovic9420rn_projekat/resources/UserResource.java
@@ -1,4 +1,7 @@
 package com.vvukovic9420rn_projekat.resources;
 
+import javax.ws.rs.Path;
+
+@Path("/users")
 public class UserResource {
 }
